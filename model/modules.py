b85737744b6cba4f69c951a9c6cca51390381820
Melekhov Iaroslav <yaroslav.melekhov@gmail.com>
2019-01-07 14:23:35 +0200

    finishing module structures

--- model/modules.py
+++ model/modules.py
@@ -76,7 +76,7 @@ class MatchabilityNet(nn.Module):
 
 class CorrespondenceMapBase(nn.Module):
     def __init__(self, in_channels, bn=False):
-
+        net = nn.Module() 
 
     def forward(self, x1, x2=None, x3=None):
         x = x1
@@ -90,3 +90,86 @@ class CorrespondenceMapBase(nn.Module):
 
         x = self.net(x)
         return x
+
+
+class CMDTop(CorrespondenceMapBase):
+    def __init__(self, in_channels, bn=False):
+        super(CMDTop, self).__init__()
+        chan = [128, 128, 96, 64, 32]
+        net = nn.Sequential(conv_blck(in_channels, chan[0], bn=bn),
+                            conv_blck(chan[0], chan[1], bn=bn),
+                            conv_blck(chan[1], chan[2], bn=bn),
+                            conv_blck(chan[2], chan[3], bn=bn),
+                            conv_blck(chan[3], chan[4], bn=bn),
+                            conv_head(chan[-1]))
+
+
+class CMD60x60(CorrespondenceMapBase):
+    def __init__(self, in_channels, bn=False):
+        super(CMD60x60, self).__init__()
+        # number of output channels
+        chan = [128, 96, 64, 32]
+        net = nn.Sequential(conv_blck(in_channels, chan[0], bn=bn),
+                            conv_blck(chan[0], chan[1], padding=2, dilation=2, bn=bn),
+                            conv_blck(chan[1], chan[2], padding=3, dilation=3, bn=bn),
+                            conv_blck(chan[2], chan[3], padding=4, dilation=4, bn=bn),
+                            conv_head(chan[-1]))
+
+
+class CMD120x120(CorrespondenceMapBase):
+    def __init__(self, in_channels, bn=False):
+        super(CMD120x120, self).__init__()
+        chan = [128, 96, 64, 32]
+        net = nn.Sequential(conv_blck(in_channels, chan[0], bn=bn),
+                            conv_blck(chan[0], chan[1], padding=4, dilation=4, bn=bn),
+                            conv_blck(chan[1], chan[2], padding=6, dilation=6, bn=bn),
+                            conv_blck(chan[2], chan[3], padding=8, dilation=8, bn=bn),
+                            conv_head(chan[-1]))
+        
+
+class CMD240x240(CorrespondenceMapBase):
+    def __init__(self, in_channels, bn=False):
+        super(CMD240x240, self).__init__()
+        chan = [128, 96, 64, 32]
+        net = nn.Sequential(conv_blck(in_channels, chan[0], bn=bn),
+                            conv_blck(chan[0], chan[1], padding=4, dilation=4, bn=bn),
+                            conv_blck(chan[1], chan[2], padding=12, dilation=12, bn=bn),
+                            conv_blck(chan[2], chan[3], padding=16, dilation=16, bn=bn),
+                            conv_head(chan[-1]))
+
+
+class VGGPyramid(nn.Module):
+    def __init__(self):
+        super(VGGPyramid, self).__init__()
+        self.n_levels = 5
+        source_model = models.vgg16(pretrained=True)
+
+        modules = OrderedDict()
+        tmp = []
+        n_ch = 0
+        first_relu = False
+
+        for c in source_model.features.children():
+            if (isinstance(c, nn.ReLU) and not first_relu) or (isintance(c, nn.MaxPool2d)):
+                first_relu = True
+                tmp.append(c)
+                modules['level_' + str(c - 1)] = nn.Sequential(*tmp)
+                for param in modules['level_' + str(c - 1)].parameters():
+                    param.requires_grad = False
+
+                tmp = []
+            else:
+                tmp.append(c)
+
+            if n_ch == self.n_levels:
+                break
+
+        self.__dict__['_modules'] = modules
+
+    def forward(self, x):
+        outputs = []
+        for layer_n in range(0, self.n_levels):
+            x = self.__dict__['_modules']['level_' + str(layer_n)](x)
+            outputs.append(x)
+
+        return outputs
