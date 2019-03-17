2bfa87189f7e7bfb0ed771a1368ae099e59c5227
imelekhov <iaroslav.melekhov@aalto.fi>
2019-03-17 14:33:40 +0200

    some updates

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import fnmatch
 import os.path as osp
 
 from model.net import DGCNet
+from utils import calculate_epe_hpatches
 
 
 def find_checkpoint(path, pattern='*.pth'):
@@ -22,8 +23,19 @@ def find_checkpoint(path, pattern='*.pth'):
 
 
 # Argument parsing
-parser = argparse.ArgumentParser(description='DGCNet')
+parser = argparse.ArgumentParser(description='DGC-Net')
+# Paths
+parser.add_argument('--csv-path', type=str, default='hpathces/data',
+                    help='path to training transformation csv folder')
+parser.add_argument('--image-path', type=str, default='/hdd/projects/widebaseline_correspondence_search/from_ssd/wide_baseline_correspondence/datasets/dtu/rect_240x240',
+                    help='path to folder containing training images')
 
+parser.add_argument('--model', type=str, default='dgc', 
+        help='Model to use', choices=['dgc', 'dgcm'])
+parser.add_argument('--metric', type=str, default='aepe', 
+        help='Model to use', choices=['aepe', 'pck'])
+parser.add_argument('--batch-size', type=int, default=1,
+                    help='evaluation batch size')
 parser.add_argument('--seed', type=int, default=1984, help='Pseudo-RNG seed')
 
 args = parser.parse_args()
@@ -31,4 +43,59 @@ args = parser.parse_args()
 torch.manual_seed(args.seed)
 torch.cuda.manual_seed(args.seed)
 
-#for epoch in range(args.n_epoch):
+# Image normalisation
+mean_vector = np.array([0.485, 0.456, 0.406])
+std_vector = np.array([0.229, 0.224, 0.225])
+normTransform = transforms.Normalize(mean_vector, std_vector)
+dataset_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        normTransform
+    ])
+
+# Model
+checkpoint_fname = osp.join('pretrained_models', args.model, 'checkpoint.pth')
+if not osp.isfile(checkpoint_fname):
+    raise ValueError('check the snapshots path')
+
+if args.model == 'dgc':
+    net = DGCNet()
+
+net.load_state_dict(torch.load(checkpoint_fname)['state_dict'])
+net = nn.DataParallel(net)
+net.eval()
+net.cuda()
+
+with torch.no_grad():
+    number_of_scenes = 5
+    if (metric == 'AEPE'):
+        res = []
+        jac = []
+    if (metric == 'PCK'):
+        threshold_range = np.linspace(0.005, 0.1, num=200)
+        res = np.zeros((number_of_scenes, len(threshold_range)))
+
+    # loop over scenes (1-2, 1-3, 1-4, 1-5, 1-6)
+    for id, k in enumerate(range(2, number_of_scenes+2)):
+        test_dataset = HPatchTestDataset(csv_file=osp.join(args.csv_path, 'hpatches_1_' + str(k) + '.csv'),
+                                         image_path=args.image_path,
+                                         transforms=dataset_transforms)
+        '''
+        test_dataset = HPatchTestDataset__M(csv_file=os.path.join(CSV_PATH, 'hpatches_1_'+str(k)+'.csv'),
+                                         image_path_orig='/ssd_storage/projects/wide_baseline_correspondence/datasets/HPatches/hpatches-geometry',
+                                         image_path=IMG_PATH,
+                                         transforms=dataset_transforms)
+        '''
+        test_dataloader = DataLoader(test_dataset, 
+                                     batch_size=args.batch_size,
+                                     shuffle=False,
+                                     num_workers=4)
+
+        if (metric == 'AEPE'):
+            epe_arr = calculate_epe_hpatches(net, test_dataloader)
+            res.append(np.mean(epe_arr))
+
+        if (metric == 'PCK'):
+            for t_id, threshold in enumerate(threshold_range):
+                res[id, t_id] = calculate_pck_hpatches(net, test_dataloader, alpha=threshold)
+
+    print(res)
---
+++ utils.py
@@ -0,0 +1,112 @@
+from tqdm import tqdm
+import itertools
+
+
+
+def epe(input_flow, target_flow):
+    """
+    End-point-Error computation
+    """
+    return torch.norm(target_flow - input_flow, p=2, dim=1).mean()
+
+
+def correct_correspondences(input_flow, target_flow, alpha, im_size=240):
+    input_flow = input_flow.unsqueeze(0)
+    target_flow = target_flow.unsqueeze(0)
+    dist = torch.norm(target_flow - input_flow,p=2,dim=0).unsqueeze(0)
+
+    pck_threshold = alpha * im_size
+    mask = dist.le(pck_threshold)
+
+    return len(dist[mask.detach()])
+
+
+def calculate_epe_hpatches(net, val_loader, img_size=240):
+    aepe_array = []
+    n_registered_pxs = 0
+
+    pbar = tqdm(enumerate(val_loader), total=len(val_loader))
+    for _, mini_batch in pbar:
+
+        source_img = mini_batch['source_image'].to(net.device())
+        target_img = mini_batch['target_image'].to(net.device())
+        bs, _, _, _ = source_img.shape
+
+        # net prediction
+        estimates_grid = net(source_img, target_img)
+
+        flow_est = estimates_grid[-1].transpose(1,2).transpose(2,3).to(net.device())
+        flow_target = mini_batch['correspondence_map'].to(net.device())
+
+        # applying mask
+        mask_x_gt = flow_target[:, :, :, 0].ge(-1) & flow_target[:, :, :, 0].le(1)
+        mask_y_gt = flow_target[:, :, :, 1].ge(-1) & flow_target[:, :, :, 1].le(1)
+        mask_xx_gt = mask_x_gt & mask_y_gt
+        mask_gt = torch.cat((mask_xx_gt.unsqueeze(3), mask_xx_gt.unsqueeze(3)), dim=3)
+
+        for i in range(bs):
+            # unnormalize the flow: [-1;1] -> [0;239]
+            flow_target[i] = (flow_target[i] + 1) * (img_size - 1) / (1 + 1)
+            flow_est[i]    = (flow_est[i] + 1) * (img_size - 1) / (1 + 1)
+
+
+        flow_target_x = flow_target[:, :, :, 0]
+        flow_target_y = flow_target[:, :, :, 1]
+        flow_est_x = flow_est[:, :, :, 0]
+        flow_est_y = flow_est[:, :, :, 1]
+
+        flow_target = torch.cat((flow_target_x[mask_gt[:, :, :, 0]].unsqueeze(1),
+                                 flow_target_y[mask_gt[:, :, :, 1]].unsqueeze(1)), dim=1)
+        flow_est = torch.cat((flow_est_x[mask_gt[:, :, :, 0]].unsqueeze(1),
+                              flow_est_y[mask_gt[:, :, :, 1]].unsqueeze(1)), dim=1)
+
+        # let's calculate EPE
+        #aepe = EPE(flow_est_m, flow_target_m)
+        aepe = epe(flow_est, flow_target)
+        aepe_array.append(aepe.item())
+        n_registered_pxs += flow_target.shape[0]
+
+    aepe_array = list(itertools.chain(*aepe_array))
+    print(n_registered_pxs)
+    return aepe_array
+
+
+def calculate_pck_hpatches(net, val_loader, alpha=1, im_size=240):
+    total_number_of_correspondences = 0
+    total_correct_correspondences = 0
+
+    pbar = tqdm(enumerate(val_loader), total=len(val_loader))
+    for _, mini_batch in pbar:
+
+        source_img = mini_batch['source_image'].to(net.device())
+        target_img = mini_batch['target_image'].to(net.device())
+
+        # network estimates
+        estimates_grid = net(source_img, target_img)
+
+        flow_est = estimates_grid[-1].transpose(1,2).transpose(2,3).to(net.device())
+        flow_target = mini_batch['correspondence_map'].to(net.device())
+        bs, ch_g, h_g, w_g = flow_target.shape
+
+        # applying mask
+        mask_x_gt = flow_target[:, :, :, 0].ge(-1) & flow_target[:, :, :, 0].le(1)
+        mask_y_gt = flow_target[:, :, :, 1].ge(-1) & flow_target[:, :, :, 1].le(1)
+        mask_xx_gt = mask_x_gt & mask_y_gt
+        mask_gt = torch.cat((mask_xx_gt.unsqueeze(3), mask_xx_gt.unsqueeze(3)), dim=3)
+
+        for i in range(bs):
+            # unnormalize the flow: [-1;1] -> [0;239]
+            flow_target[i] = (flow_target[i] + 1) * (img_size - 1) / (1 + 1)
+            flow_est[i]    = (flow_est[i] + 1) * (img_size - 1) / (1 + 1)
+
+        flow_target = flow_target.contiguous().view(1, bs * h_g * w_g * ch_g)
+        flow_est    = flow_est.contiguous().view(1, bs * h_g * w_g * ch_g)
+        # applying mask
+        flow_target_m = flow_target[mask_gt.contiguous().view(1, bs * h_g * w_g * ch_g)]
+        flow_est_m    = flow_est[mask_gt.contiguous().view(1, bs * h_g * w_g * ch_g)]
+
+        total_number_of_correspondences += len(flow_target_m)
+        total_correct_correspondences += correct_correspondences(flow_est_m, flow_target_m, alpha=alpha, im_size=im_size)
+
+    pck = total_correct_correspondences / (total_number_of_correspondences + 1e-6)
+    return pck
